c74913beceebfefc6975f5a3a5d1e26227325c49
Vinicius Ribeiro da Silva do Carmo <viniciusjhu@gmail.com>
2025-12-01 11:52:54 -0300

    Feat: Implementa US-1307 Edição de Categoria com API REST e Frontend Integrado

--- src/main/java/com/musicstore/bluevelvet/api/request/CategoryRequest.java
+++ src/main/java/com/musicstore/bluevelvet/api/request/CategoryRequest.java
@@ -2,6 +2,7 @@ package com.musicstore.bluevelvet.api.request;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
@@ -9,4 +10,6 @@ public class CategoryRequest {
     private String name;
     private String description;
     private Long parentCategoryId;
+    private MultipartFile imageFile;
+    private Boolean enabled;
 }
